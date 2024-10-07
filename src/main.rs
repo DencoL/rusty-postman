8bf6b49d769893e6f919dca326d1812dc09a845c
DencoL <denis@lochman.sk>
2024-10-07 15:27:56 +0200

    feat: add basic ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+mod tui;
+
+fn main() -> color_eyre::Result<()> {
+    tui::install_panic_hook();
+    let mut _terminal = tui::init_terminal()?;
+
+    tui::restore_terminal()?;
+
+    Ok(())
 }
